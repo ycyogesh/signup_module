09bc2d72d8fdeaf12d4d0a1d0da7a6cb3778a7ad
ycyogesh <ycyogesh183@gmail.com>
2022-08-25 01:18:01 +0530

    added codes

--- signServer.js
+++ signServer.js
@@ -1,51 +1,62 @@
-const express = require("express")
-const bcrypt = require('bcrypt');
-const cors = require("cors")
-const mysql = require("mysql")
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const check = require("express-validator");
 const saltRounds = 10;
-app = express()
-app.use(cors())
-app.use(express.json())
-
-
+app = express();
+app.use(cors());
+app.use(express.json());
 
 var connection = mysql.createConnection({
-    host : "localhost",
-    user : "nodejs",
-    password : "Node@123",
-    database : "crud"
+  host: "localhost",
+  user: "nodejs",
+  password: "Node@123",
+  database: "crud",
 });
 
-connection.connect(function(err){
-    if(err){
-        console.log("error connecting" + err.stack);
-        return;
-    }
-    console.log("connected as id :", + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.log("error connecting" + err.stack);
+    return;
+  }
+  console.log("connected as id :", +connection.threadId);
 });
 
 
 
 
 
-app.post("/signUp",(req,res)=>{
+app.post("/signUp", (req, res) => {
+  let data = req.body;
 
-    let data = req.body;
-
-    bcrypt.genSalt(10, (err, salt) => {
-        console.log("----------->");
-        bcrypt.hash(data.pwd, salt, function(err, hash) {
-            let sql = "insert into user(email,passwrd) values(?,?)";
-            connection.query(sql,[data.email,hash],(err,result)=>{
-        if(err){
-            console.log("Error");
+  bcrypt.genSalt(10, (err, salt) => {
+    console.log("----------->");
+    bcrypt.hash(data.pwd, salt, function (err, hash) {
+      let sql = "insert into user(email,passwrd) values(?,?)";
+      connection.query(sql, [data.email, hash], (err, result) => {
+        if (err) {
+          console.log("Error");
         }
-        res.json({result})
-    })
+        res.json({ result });
+      });
+    });
+  });
+});
+
+
+
+app.post("/logIn",(req,res)=>{
+    let data = req.body;
+    bcrypt.compare(data.pwd, hash, function(err, result) {
+        if (result) {
+           window.location = "form_operations.html"
+       }
+    });
 })
-        });
-    })
-    
-app.listen(3010,()=>{
-    console.log("App Running");
-})
+
+
+app.listen(3010, () => {
+  console.log("App Running");
+});
