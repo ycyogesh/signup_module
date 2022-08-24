26aaddfd6993494e6d96d0b99754f80deb9a8ece
ycyogesh <ycyogesh183@gmail.com>
2022-08-25 00:51:41 +0530

    added codes

--- signServer.js
+++ signServer.js
@@ -1,4 +1,51 @@
 const express = require("express")
 const bcrypt = require('bcrypt');
+const cors = require("cors")
+const mysql = require("mysql")
 const saltRounds = 10;
 app = express()
+app.use(cors())
+app.use(express.json())
+
+
+
+var connection = mysql.createConnection({
+    host : "localhost",
+    user : "nodejs",
+    password : "Node@123",
+    database : "crud"
+});
+
+connection.connect(function(err){
+    if(err){
+        console.log("error connecting" + err.stack);
+        return;
+    }
+    console.log("connected as id :", + connection.threadId);
+});
+
+
+
+
+
+app.post("/signUp",(req,res)=>{
+
+    let data = req.body;
+
+    bcrypt.genSalt(10, (err, salt) => {
+        console.log("----------->");
+        bcrypt.hash(data.pwd, salt, function(err, hash) {
+            let sql = "insert into user(email,passwrd) values(?,?)";
+            connection.query(sql,[data.email,hash],(err,result)=>{
+        if(err){
+            console.log("Error");
+        }
+        res.json({result})
+    })
+})
+        });
+    })
+    
+app.listen(3010,()=>{
+    console.log("App Running");
+})
--- signup.sql
+++ signup.sql
@@ -1,3 +1,11 @@
 -- use crud; 
-create table user(id int(11) primary key not null auto_increment,email varchar(255),passwrd password ( string_to_encrypt )
-)
+-- create table user(id int(11) primary key not null auto_increment,
+-- email varchar(255),
+-- passwrd varchar(255),
+-- is_verified int(11) default 0,
+-- token varchar(255),
+-- is_deleted boolean default 0,
+-- created_by int(11) default 1,
+-- updated_by int(11),
+-- created_at datetime default current_timestamp,
+-- updated_at datetime);
