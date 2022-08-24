7b4d07a4adbcc987b5f46c79144dc033712a7a97
ycyogesh <ycyogesh183@gmail.com>
2022-08-25 00:07:28 +0530

    added codes

---
+++ signServer.js
@@ -0,0 +1,4 @@
+const express = require("express")
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+app = express()
---
+++ signup.sql
@@ -0,0 +1,3 @@
+-- use crud; 
+create table user(id int(11) primary key not null auto_increment,email varchar(255),passwrd password ( string_to_encrypt )
+)
